3f2388e822689e3a449e317c15e60712c5f07662
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-06-05 12:08:08 +0300

    some funcs and properties were added

--- wlr ninja/GameScene.swift
+++ wlr ninja/GameScene.swift
@@ -10,9 +10,45 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-  
-    override func didMove(to view: SKView) {
-        
+    var gameScore:SKLabelNode!
     
+    var score = 0{
+        didSet{
+            gameScore.text = "Score: \(score)"
         }
+    }
+    
+    var livesImages = [SKSpriteNode]()
+    var lives = 3
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "sliceBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        
+        
+        physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+        physicsWorld.speed = 0.85
+        
+        createLives()
+        createScore()
+        createSlices()
+    }
+    
+    func createScore(){
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        addChild(gameScore)
+        gameScore.position = CGPoint(x: 8, y: 8)
+        score = 0
+        
+    }
+    func createLives(){
+        
+    }
+    
+    func createSlices(){
+        
+    }
 }
